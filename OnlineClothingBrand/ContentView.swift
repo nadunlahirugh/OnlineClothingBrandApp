8b5699f7e55c2ce5e19eab46f75c73d1011bfdd5
nadunlahirugh <nadunlahirugh1994@gmail.com>
2024-03-25 21:51:21 +0530

    Initial Commit

---
+++ OnlineClothingBrand/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlineClothingBrand
+//
+//  Created by Akvasoft Pvt Ltd on 2024-03-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlineClothingBrand/OnlineClothingBrandApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineClothingBrandApp.swift
+//  OnlineClothingBrand
+//
+//  Created by Akvasoft Pvt Ltd on 2024-03-25.
+//
+
+import SwiftUI
+
+@main
+struct OnlineClothingBrandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
